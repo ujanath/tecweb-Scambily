fd06313abbb11ecd91751cfabc9a0c9be5b43c98
Janat <250729@studenti.unimore.it>
2022-09-03 04:27:32 +0200

    test metodo_pagamento_carta

--- ecommerce/core/portafoglio/tests.py
+++ ecommerce/core/portafoglio/tests.py
@@ -1,3 +1,30 @@
+import unittest
+from django.contrib.auth.models import User
+
 from django.test import TestCase
+from .models import metodo_pagamento_carta
 
 # Create your tests here.
+
+class Test_metodo_pagamento_carta(unittest.TestCase):
+
+    def setUp(self):
+
+        user = User.objects.create(username='testuser')
+        user.set_password('12345678')
+        user.save()
+
+        self.pagamento1 = metodo_pagamento_carta.objects.create(
+            user = user ,
+            carta_codice= '01234567890123456789',
+            carta_CVV='123',
+            carta_scadenza_mese='12',
+            carta_scadenza_giorno='10',
+
+        )
+
+
+    def test_codice_carta(self):
+        test = 'finisce per *789'
+        self.assertEqual(self.pagamento1.ultime_tre_cifre(), test)
+
